a11cd80b1febbfff70b725b18619d27eb53b2943
Pavel <ordaz.pavel@gmail.com>
2019-09-01 15:23:47 +0300

    chanages

--- install.lua
+++ install.lua
@@ -1,5 +1,16 @@
 component = require("component")
+filesystem = require("filesystem")
+os = requrie("os")
 
 redstone = component.redstone
 redstone.setWakeThreshold(1)
 
+if (filesystem.exists("/home/casino")) then
+    filesystem.remove("/home/casino/casino.lua")
+else
+    os.execute("mkdir /home/casino")
+    os.execute("cp casino/configcasino.lua /home/casino/configcasino.lua")
+    os.execute("cp casino/data-casino.dat /home/casino/data-casino.dat")
+end
+
+os.execute("cp casino/casino.lua /home/casino/casino.lua")
